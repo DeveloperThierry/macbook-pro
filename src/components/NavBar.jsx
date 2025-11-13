75ed29c2c046eee6b1e03c397d34a304d9734ca3
developerthierry <developerthierry@gmail.com>
2025-11-13 20:03:09 +0000

    NavBar Component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { navlinks } from "../constants";
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="Apple logo" />
+        <ul>
+          {navlinks.map(({ label }) => (
+            <li key={label}>
+              <a href={label}>{label}</a>
+            </li>
+          ))}
+        </ul>
+        <div classlabel="flex-center gap-3">
+            <button>
+                <img src="/search.svg" alt="Search" />
+            </button>
+            <button>
+                <img src="/cart.svg" alt="Cart" />
+            </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
