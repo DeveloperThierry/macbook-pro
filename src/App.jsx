3a2385087862b59ec49e031afa662ec8aa55410a
developerthierry <developerthierry@gmail.com>
2025-11-14 01:13:06 +0000

    Implement Showcase Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Hero from "./components/Hero";
 import ProductViewer from "./components/ProductViewer";
 import gsap from 'gsap'
 import {ScrollTrigger, SplitText} from 'gsap/all'
+import Showcase from "./components/Showcase";
 
 gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
   return (
-    <main>
+    <main className="flex flex-col gap-10">
       <NavBar />
       <Hero />
       <ProductViewer />
+      <Showcase/>
     </main>
   );
 };
